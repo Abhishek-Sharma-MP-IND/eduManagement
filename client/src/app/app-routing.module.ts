3a80b5f2e73b9b63a1475f7c06eb05fa8acb3cf7
username <wecp-dummy@gmail.com>
2025-02-14 08:28:26 +0000

    new commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RegistrationComponent } from './registration/registration.component';
 
 
 import { AppComponent } from './app.component';
-import { DashbaordComponent } from './dashbaord/dashbaord.component';
+import { DashboardComponent } from './dashbaord/dashbaord.component';
 
 import { CreateEventComponent } from './create-event/create-event.component';
 import { AddResourceComponent } from './add-resource/add-resource.component';
@@ -18,16 +18,16 @@ import { RegisterForEventComponent } from './register-for-event/register-for-eve
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
-  { path: 'dashboard', component: DashbaordComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'create-event', component: CreateEventComponent },  
   { path: 'add-resource', component: AddResourceComponent }, 
   { path: 'resource-allocate', component: ResourceAllocateComponent },  
   { path: 'view-events', component: ViewEventsComponent },  
   { path: 'booking-details', component: BookingDetailsComponent }, 
   { path: 'register-for-event', component: RegisterForEventComponent },   
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 
-  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 import { HttpService } from '../../services/http.service';
 
 
@@ -9,7 +10,47 @@ import { HttpService } from '../../services/http.service';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
-//todo: complete missing code..
+export class RegistrationComponent implements OnInit {
+  itemForm!: FormGroup;
+  formModel: any;
+  showMessage: boolean = false;
+  responseMessage: any;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpService: HttpService,
+
+    private authService: AuthService,
+    private route :Router
+  ) {}
+
+  ngOnInit(): void {
+    this.itemForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: [null, Validators.required],
+      username: ['', Validators.required]
+    });
+  }
+
+  onRegister(): void {
+    if (this.itemForm.valid) {
+      this.formModel = this.itemForm.value;
+      // Call HTTP service to register
+      this.httpService.registerUser(this.formModel).subscribe(
+        (response) => {
+
+          this.responseMessage = "Registration successfull!";
+          this.showMessage = true;
+          setTimeout(() => {
+            this.route.navigateByUrl('/login');
+          }, 2000);
+        },
+        (error) => {
+          this.responseMessage = error;
+          this.showMessage = true;
+        }
+      );
+    }
+  }
 }
